32a704043b2676e4dff998b5d1517ef90c68a522
Meghna-24 <meghnamadhu24@gmail.com>
2024-11-12 18:29:35 +0530

    Add files via upload

---
+++ octa_task2.js
@@ -0,0 +1,127 @@
+
+function enableCheckbox1() {
+    var checkbox = document.getElementById("t1");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+function enableCheckbox2() {
+    var checkbox = document.getElementById("t2");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+
+function enableCheckbox3() {
+    var checkbox = document.getElementById("t3");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+
+function enableCheckbox4() {
+    var checkbox = document.getElementById("t4");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+
+function enableCheckbox5() {
+    var checkbox = document.getElementById("t5");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+function enableCheckbox6() {
+    var checkbox = document.getElementById("t6");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+
+function enableCheckbox7() {
+    var checkbox = document.getElementById("t7");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+
+function enableCheckbox8() {
+    var checkbox = document.getElementById("t8");
+    if (checkbox.checked) {
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = true;
+    }
+}
+
+function clearInput1() {
+    document.getElementById('task1').value = '';
+    document.getElementById("t1").checked = false;
+}
+function clearInput2() {
+    document.getElementById('task2').value = '';
+    document.getElementById("t2").checked = false;
+}
+function clearInput3() {
+    document.getElementById('task3').value = '';
+    document.getElementById("t3").checked = false;
+}
+function clearInput4() {
+    document.getElementById('task4').value = '';
+    document.getElementById("t4").checked = false;
+}
+function clearInput5() {
+    document.getElementById('task5').value = '';
+    document.getElementById("t5").checked = false;
+}
+function clearInput6() {
+    document.getElementById('task6').value = '';
+    document.getElementById("t6").checked = false;
+}
+function clearInput7() {
+    document.getElementById('task7').value = '';
+    document.getElementById("t7").checked = false;
+}
+function clearInput8() {
+    document.getElementById('task8').value = '';
+    document.getElementById("t8").checked = false;
+}
+
+function showList1() {
+    var textBoxValue = document.getElementById('task1').value;
+    document.getElementById('1').innerText = textBoxValue;
+
+}
+function showList2() {
+    var textBoxValue = document.getElementById('task2').value;
+    document.getElementById('2').innerText = textBoxValue;
+
+}
+function showList3() {
+    var textBoxValue = document.getElementById('task3').value;
+    document.getElementById('3').innerText = textBoxValue;
+
+}
